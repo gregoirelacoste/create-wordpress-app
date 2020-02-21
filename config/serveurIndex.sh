f9d5ffaeecd405518df9b1753007df9ab756ddf8
Lacoste <gregoire@level-one.fr>
2020-02-21 12:43:52 +0100

    first

---
+++ config/serveurIndex.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+HEADERFILE=$dossierServeur/lv1_header.php
+INDEXFILE=$dossierServeur/lv1_index.php
+nouveaulien="<li><a href='$projectname'>Wordpress $projectname</a></li>"
+
+indexcontent="<html>
+<head>
+  <title>Home Serveur LV1</title>
+</head>
+<body>
+<h1>LV1 Create Wordpress App</h1>
+<p>Choisissez votre site dans la liste ci-dessous.</p>
+<?php include ('lv1_header.php'); ?>
+</body>
+</html>
+"
+headercontent="<ul>$nouveaulien</ul>"
+
+createLinkOrFile(){
+	cd $dossierServeur
+	echo $HEADERFILE
+	pwd
+	if [ -f "$HEADERFILE" ]
+		then
+		    echo "Ajout du lien dans $HEADERFILE"
+		    sed -i "/<\/ul>/i\ $nouveaulien" $HEADERFILE
+		else 
+		    echo "Création de $HEADERFILE et $INDEXFILE"
+		    touch $INDEXFILE
+		    touch $HEADERFILE
+		    echo $indexcontent >> $INDEXFILE
+			echo $headercontent >> $HEADERFILE
+	fi
+}
---
+++ config/variables.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+projectname=$1
+dossierServeur=/home/$USER/dev/lamp
+serveur=localhost
+port=7777
+navigateur=firefox
+dbname=wpdb_$projectname
+userDBname=user_$projectname
