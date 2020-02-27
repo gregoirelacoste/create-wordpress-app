d31fc64892f10ce3646626c6a47dec0a46f93c8e
Lacoste <gregoire@level-one.fr>
2020-02-27 16:10:40 +0100

    change text tpl

--- install/theme/header.php
+++ install/theme/header.php
@@ -4,8 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <?php wp_head(); ?>
-  <meta name="description" content="Réseau de plus de 500 photographes
-            professionnels pour les cabinets d’expertise." />
+  <meta name="description" content="Created with Create Wordpress App by Gregoire Lacoste" />
 </head>
 
 <body <?php body_class(); ?>>
@@ -16,22 +15,9 @@
         <img src="<?= get_template_directory_uri(); ?>/img/logo.svg" alt="logo" height="50px" />
       </a>
     </div>
-    <nav id="cwapp_main_menu" class="navigation m-hide">
+    <nav id="cwapp_main_menu" class="navigation">
         <?php wp_nav_menu( array( 'theme_location' => 'main','container' => 'ul') ); ?>
     </nav>
-    <div class="d-hide">
-      <nav id="cwapp_main_menu">
-        <h2>SnapClaim</h2>
-        <?php wp_nav_menu( array( 'theme_location' => 'main','container' => 'ul') ); ?>
-      </nav>
-    </div>
-    <div class="d-hide">
-      <div id="menu-burger">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-      </div>
-    </div>
   </section>
 </header>
 
