1e8ec16dae156fad680a818c3af60e2daf121134
Lacoste <gregoire@level-one.fr>
2020-03-02 22:36:47 +0100

    user docker for mysql

--- install/coreWP.sh
+++ install/coreWP.sh
@@ -11,9 +11,9 @@ installCoreWP(){
     echo "Téléchargement et Installation de WP"
     wp core download --locale=`conf wordpress.install.lang`
     echo "configuration de wp-config"
-    wp config create --dbname=`conf database.name` --dbuser=`conf database.user` --dbpass=`conf database.password` --dbhost=`conf database.host` --dbprefix=`conf database.prefix` --extra-php=$debug
+    wp config create --dbname=`conf database.name` --dbuser=`conf database.user` --dbpass=`conf database.password` --dbhost="$(conf database.host)" --dbprefix=`conf database.prefix` --extra-php=$debug
     echo "Core install de WP"
-    wp core install --url=`conf wordpress.url` --title="`conf wordpress.title`" --admin_user=`conf wordpress.install.user` --admin_password="`conf wordpress.install.password`" --admin_email=`conf wordpress.install.email`
+    wp core install --url=`conf wordpress.url` --title="`conf wordpress.title`" --admin_user="$(conf wordpress.install.user)" --admin_password="`conf wordpress.install.password`" --admin_email=`conf wordpress.install.email`
     wp option update siteurl http://`conf wordpress.url`
     wp option update home http://`conf wordpress.url`
 }
--- install/database.sh
+++ install/database.sh
@@ -1,7 +1,42 @@
+dockerComposeContent="version: '3.3'
+services:
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: $(conf database.name)
+      MYSQL_USER: $(conf database.user)
+      MYSQL_PASSWORD: $(conf database.password)
+      MYSQL_ROOT_PASSWORD: $(conf database.password)
+    ports:
+      - '$(conf database.port):3306'
+    expose:
+      - '$(conf database.port)'
+    volumes:
+      - $(conf database.name):/var/lib/mysql
+volumes:
+  $(conf database.name):"
+
+localMysql(){
+  echo "local sql with sudo"
+  sudo mysql -e "CREATE DATABASE `conf database.name` /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+  sudo mysql -e "CREATE USER `conf database.user`@`conf database.host` IDENTIFIED BY '`conf database.password`';"
+  sudo mysql -e "GRANT ALL PRIVILEGES ON `conf database.name`.* TO '`conf database.user`'@'`conf database.host`';"
+  sudo mysql -e "FLUSH PRIVILEGES;"
+}
+
+dockerMysql(){
+  echo "docker-compose"
+  touch docker-compose.yml && echo $dockerComposeContent > docker-compose.yml
+  docker-compose up -d
+}
+
 createDatabase(){
     echo "Create DB"
-    sudo mysql -e "CREATE DATABASE `conf database.name` /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    sudo mysql -e "CREATE USER `conf database.user`@`conf database.host` IDENTIFIED BY '`conf database.password`';"
-    sudo mysql -e "GRANT ALL PRIVILEGES ON `conf database.name`.* TO '`conf database.user`'@'`conf database.host`';"
-    sudo mysql -e "FLUSH PRIVILEGES;"
-    }
+    if [ "$(conf database.docker)" ]
+      then
+       dockerMysql
+      else
+        localMysql
+      fi
+}
