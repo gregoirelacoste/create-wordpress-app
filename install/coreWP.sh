f5ceb1d91fc8297ba39387bfa8e9ca5c201772bc
Lacoste <gregoire@level-one.fr>
2020-03-03 00:23:45 +0100

    catch error on corewp install

--- install/coreWP.sh
+++ install/coreWP.sh
@@ -6,14 +6,22 @@ debug(){
      debug="define( 'WP_DEBUG', false ); define( 'WP_DEBUG_LOG', false );"
   fi
 }
+wpConfig(){
+    wp config create --dbname=`conf database.name` --dbuser=`conf database.user` --dbpass=`conf database.password` --dbhost="$(conf database.host)" --dbprefix=`conf database.prefix` --extra-php=$debug
+}
 
 installCoreWP(){
     echo "Téléchargement et Installation de WP"
     wp core download --locale=`conf wordpress.install.lang`
     echo "configuration de wp-config"
-    wp config create --dbname=`conf database.name` --dbuser=`conf database.user` --dbpass=`conf database.password` --dbhost="$(conf database.host)" --dbprefix=`conf database.prefix` --extra-php=$debug
-    echo "Core install de WP"
-    wp core install --url=`conf wordpress.url` --title="`conf wordpress.title`" --admin_user="$(conf wordpress.install.user)" --admin_password="`conf wordpress.install.password`" --admin_email=`conf wordpress.install.email`
-    wp option update siteurl http://`conf wordpress.url`
-    wp option update home http://`conf wordpress.url`
+    if wpConfig
+     then
+      echo "Core install de WP"
+      wp core install --url=`conf wordpress.url` --title="`conf wordpress.title`" --admin_user="$(conf wordpress.install.user)" --admin_password="`conf wordpress.install.password`" --admin_email=`conf wordpress.install.email`
+      wp option update siteurl http://`conf wordpress.url`
+      wp option update home http://`conf wordpress.url`
+     else
+       echo "erreur"
+       set -e
+     fi
 }
